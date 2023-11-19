8ce44b3ba93fa734aa363300d7361ca85d0ffcc9
bendodag <db@Dagmars-MacBook-Air.local>
2023-11-19 22:29:59 +0100

    diff payment + overpayment

--- loan_calculator.py
+++ loan_calculator.py
@@ -2,6 +2,11 @@ import math
 import argparse
 import sys
 
+
+def overpayment(total, principal):
+    print(f"Overpayment = {math.ceil(total - principal)}")
+
+
 parser = argparse.ArgumentParser(description="This program calculates your loan properties based on your input.")
 parser.add_argument("--type", type=str)
 parser.add_argument("-pa", "--payment", type=float)
@@ -14,64 +19,61 @@ loan_type = args.type
 loan_principal = args.principal
 monthly_payment = args.payment
 interest = args.interest
-months = args.periods
+n_months = args.periods
 
-
-if loan_type is None or loan_type not in ["annuity", "diff"] or interest is None:
+if loan_type is None or loan_type not in ["annuity", "diff"] or interest is None or len(sys.argv) <= 4:
     print("Incorrect parameters")
     sys.exit()
 
-if len(sys.argv) <= 4:
-    print("Incorrect number of parameters")
-    sys.exit()
-
 if loan_type == "diff" and monthly_payment is not None:
-    print("Incorrect diff option parameters")
+    print("Incorrect parameters")
     sys.exit()
 
-if any(arg is not None and arg < 0 for arg in [loan_principal, monthly_payment, interest, months]):
-    print("Incorrect values of parameters, can't be < 0")
+if any(arg is not None and arg < 0 for arg in [loan_principal, monthly_payment, interest, n_months]):
+    print("Incorrect parameters")
     sys.exit()
 
+total_amount = 0
+nom_interest = interest / (12 * 100)
 
 if loan_type == "diff":
-    pass
+
+    for month in range(1, n_months + 1):
+        diff_payment = math.ceil(loan_principal / n_months + nom_interest * (loan_principal - loan_principal * (month - 1) / n_months))
+        total_amount += diff_payment
+        print(f"Month {month}: payment is {diff_payment}")
+
+    overpayment(total_amount, loan_principal)
 
 if loan_type == "annuity":
 
-    if months is None:
-
-        nom_interest = interest / (12 * 100)
-        months = math.ceil(
+    if n_months is None:
+        n_months = math.ceil(
             math.log(monthly_payment / (monthly_payment - nom_interest * loan_principal), 1 + nom_interest))
-        if months == 1:
+        if n_months == 1:
             print("It will take 1 month to repay the loan")
-        elif months in range(2, 12):
-            print(f'It will take {months} months to repay the loan')
-        elif months == 12:
+        elif n_months in range(2, 12):
+            print(f'It will take {n_months} months to repay the loan')
+        elif n_months == 12:
             print("It will take 1 year to repay the loan")
-        elif months % 12 == 0:
-            print(f'It will take {months / 12} years to repay the loan')
+        elif n_months % 12 == 0:
+            print(f'It will take {int(n_months / 12)} years to repay the loan')
         else:
-            print(f'It will take {math.floor(months / 12)} years and {months % 12} months to repay the loan')
+            print(f'It will take {math.floor(n_months / 12)} years and {n_months % 12} months to repay the loan')
+        overpayment(n_months * monthly_payment, loan_principal)
 
     elif monthly_payment is None:
-
-        nom_interest = interest / (12 * 100)
-        monthly_payment = loan_principal * nom_interest * pow((1 + nom_interest), months) / (
-                    pow(1 + nom_interest, months) - 1)
-
-        print(f'Your monthly payment = {math.ceil(monthly_payment)}!')
+        monthly_payment = math.ceil(loan_principal * nom_interest * pow((1 + nom_interest), n_months) / (
+                pow(1 + nom_interest, n_months) - 1))
+        print(f'Your monthly payment = {monthly_payment}!')
+        overpayment(n_months * monthly_payment, loan_principal)
 
     elif loan_principal is None:
-
-        nom_interest = interest / (12 * 100)
-        loan_principal = monthly_payment * (pow(1 + nom_interest, months) - 1) / (
-                    nom_interest * pow((1 + nom_interest), months))
-
-        print(f'Your loan principal = {round(loan_principal)}!')
+        loan_principal = monthly_payment * (pow(1 + nom_interest, n_months) - 1) / (
+                    nom_interest * pow((1 + nom_interest), n_months))
+        print(f'Your loan principal = {math.floor(loan_principal)}!')
+        overpayment(n_months * monthly_payment, loan_principal)
 
     else:
         print("Unexpected behavior. Option is not implemented.")
-        pass
 
